79f5944edca81fcca863c3bae9709bc928a0103b
mvolkov <mvolkov@bk.ru>
2024-01-11 01:20:50 +0300

    Файл с фото котов и кошек в ответ на апдейты

---
+++ Send_cat.py
@@ -0,0 +1,38 @@
+import requests
+import time
+
+API_URL = 'https://api.telegram.org/bot'
+BOT_TOKEN = '6761039613:AAGLNqQ0wapzYUvCXvPMG1Wib1E0bGnoc8U'
+TEXT = 'Ура! Классный апдейт !'
+MAX_COUNTER = 100
+
+API_CATS_URL = 'https://api.thecatapi.com/v1/images/search'
+ERROR_TEXT = 'Здесь должна быть картинка с котиком : ('
+
+offset = -2
+counter = 0
+
+cat_response: requests.Response
+cat_link: str
+
+while counter < 100:
+    print("attempt = ", counter)
+
+    updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset+1}').json()
+    if updates['result']:
+        # print('There is update!!!')
+        for result in updates['result']:
+            offset = result['update_id']
+            chat_id = result['message']['from']['id']
+
+            cat_response = requests.get(API_CATS_URL)
+            if cat_response.status_code == 200:
+                cat_link = cat_response.json()[0]['url']
+                requests.get(f'{API_URL}{BOT_TOKEN}/sendPhoto?chat_id={chat_id}&photo={cat_link}')
+            else:
+                 requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={ERROR_TEXT}')
+
+
+    time.sleep(1)
+    counter += 1
+
